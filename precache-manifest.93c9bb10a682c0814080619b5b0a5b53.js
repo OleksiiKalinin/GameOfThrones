f2af742c9c33fa425def9201ef1080deea3b90ff
Oleksii <svjjdnv@gmail.com>
2021-04-10 15:39:52 +0200

    Updates

---
+++ precache-manifest.93c9bb10a682c0814080619b5b0a5b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5835552b3a18ca048023",
+    "url": "/GameOfThrones/static/css/main.393c3128.chunk.css"
+  },
+  {
+    "revision": "5835552b3a18ca048023",
+    "url": "/GameOfThrones/static/js/main.5835552b.chunk.js"
+  },
+  {
+    "revision": "cae87e2a80f2c45eb3db",
+    "url": "/GameOfThrones/static/css/1.f0cbd390.chunk.css"
+  },
+  {
+    "revision": "cae87e2a80f2c45eb3db",
+    "url": "/GameOfThrones/static/js/1.cae87e2a.chunk.js"
+  },
+  {
+    "revision": "ab5973605820d40d8279",
+    "url": "/GameOfThrones/static/js/runtime~main.ab597360.js"
+  },
+  {
+    "revision": "ea419b7a45d439914a1de6c95e36e79f",
+    "url": "/GameOfThrones/index.html"
+  }
+];
